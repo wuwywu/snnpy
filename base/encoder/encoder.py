c9961580029d5f1c97ace288fd256b4bffbc042f
wuwywu <676425940@qq.com>
2023-11-02 22:09:47 +0800

    改变编码器位置

--- base/encoder/encoder.py
+++ base/encoder/encoder.py
@@ -7,12 +7,14 @@
 
 import os
 import sys
+
 sys.path.append(os.path.dirname(__file__))  # 将文件所在地址放入系统调用地址中
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 class encoder(nn.Module):
     """
@@ -25,49 +27,80 @@ class encoder(nn.Module):
     return:
         数据维度: (N, C, H, W, T)
     """
+
     def __init__(self, schemes=1, time_window=5):
         super().__init__()
         self.time_window = time_window
-        if schemes==1:  # 直流编码
+        if schemes == 1:  # 直流编码
             self.encoding = self.current_coding
 
-        if schemes==2:  # 泊松编码
+        if schemes == 2:  # 泊松编码
             self.encoding = self.Poisson_coding
 
-        if schemes==3:  # Rate-Syn coding
+        if schemes == 3:  # Rate-Syn coding
             self.encoding = self.Rate_Syn
 
-        if schemes==4:  # TTFS coding (Time-to-first-spike coding)
+        if schemes == 4:  # TTFS coding (Time-to-first-spike coding)
             self.encoding = self.TTFS
- 
-    def forward(self, x):
-        return self.encoding(x) # (N, C, H, W, T)
 
+    def forward(self, x):
+        return self.encoding(x)  # (N, C, H, W, T)
+
+    @torch.no_grad()
     def current_coding(self, x):
         # necessary for general dataset: broadcast input
-        x, _ = torch.broadcast_tensors(x, torch.zeros( (self.time_window,) + x.shape, device=x.device))  # 函数无法在最后一个维度上进行广播
+        x, _ = torch.broadcast_tensors(x,
+                                       torch.zeros((self.time_window,) + x.shape, device=x.device))  # 函数无法在最后一个维度上进行广播
         return x.permute(1, 2, 3, 4, 0).detach()
 
+    @torch.no_grad()
     def Poisson_coding(self, x):
         x_ = torch.zeros(x.shape + (self.time_window,), device=x.device)
         for step in range(self.time_window):
-            x_[...,step] = x > torch.rand(x.shape, device=x.device)
+            x_[..., step] = x > torch.rand(x.shape, device=x.device)
         return x_.detach()
-    
+
+    @torch.no_grad()
     def Rate_Syn(self, x):
         x_ = torch.zeros(x.shape + (self.time_window,), device=x.device)
-        t = ((1-x)*self.time_window).int()
+        t = ((1 - x) * self.time_window).int()
         for step in range(self.time_window):
-            x_step = torch.where(step>=t, 1., 0.)
-            x_[...,step] = x_step
+            x_step = torch.where(step >= t, 1., 0.)
+            x_[..., step] = x_step
         return x_.detach()
 
+    @torch.no_grad()
     def TTFS(self, x):
         x_ = torch.zeros(x.shape + (self.time_window,), device=x.device)
-        t = ((1-x)*self.time_window).int()
+        t = ((1 - x) * self.time_window).int()
         for step in range(self.time_window):
-            x_step = torch.where(step==t, 1., 0.)
-            x_[...,step] = x_step
+            x_step = torch.where(step == t, 1., 0.)
+            x_[..., step] = x_step
         return x_.detach()
-    
 
+
+@torch.no_grad()
+def rate(x, time_window=5):
+    """
+    reference: https://doi.org/10.1113/jphysiol.1926.sp002308.
+    Rate Coding(Poisson_coding)
+    :param x: 输入张量
+    :return:
+    描述：
+        速率编码主要基于尖峰计数，以保证时间窗口内发出的尖峰数量与真实值相对应。
+        泊松分布可以描述单位时间内发生的随机事件的数量，它对应于放电率。
+    """
+    shape = x.shape + (time_window,)
+    x, _ = torch.broadcast_tensors(x,
+                                   torch.zeros((time_window,) + x.shape, device=x.device))
+    num_dims = len(shape)
+    dim_order = [num_dims - 1] + list(range(num_dims - 1))
+    x = x.permute(*dim_order)
+    # print(x)
+    return (x > torch.rand(shape, device=x.device)).float()
+
+
+if __name__ == "__main__":
+    # encoder = encoder(3)
+    # print(encoder(torch.tensor([.1, .2, .3])))
+    print(rate(torch.tensor([1, .5, .3])))

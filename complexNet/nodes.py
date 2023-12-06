ee9cb226071a5cae78e61ea76207f6ea0b4e8b49
wuwywu <676425940@qq.com>
2023-12-06 21:28:35 +0800

    添加了Iz和FHN节点模型

--- complexNet/nodes.py
+++ complexNet/nodes.py
@@ -19,3 +19,4 @@ import matplotlib.pyplot as plt
 from base.nodes.NeuNodes import BaseNode
 from base.nodes.NeuNodes import HHnode
 from base.nodes.NeuNodes import IzhNode
+from base.nodes.NeuNodes import FHNode

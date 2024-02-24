7ac04953ab833aa90d930d1c5eed3d0c4ea91518
wuwywu <676425940@qq.com>
2024-02-24 19:35:58 +0800

    常用工具

---
+++ models/utils/utils.py
@@ -0,0 +1,150 @@
+# wuyong@ccnu
+# 一些工具
+
+# device = "gpu"
+device = "cpu"
+# from settings import *
+import numpy
+import cupy
+if device == "gpu":
+    np1 = numpy
+    np = cupy
+else:   
+    np1 = numpy
+    np = numpy
+import matplotlib.pyplot as plt
+
+# noise产生器
+class noise_types:
+    def __init__(self, size, delta_t, type="white"):
+        self.size = size
+        self.dt = delta_t
+        self.type = type  
+        if type == "color" : self.color_init = False
+
+    def __call__(self, D_noise=0., lam_color=0.1):
+        if self.type=="white":   noise = np.random.normal(loc=0., scale=np.sqrt(2*D_noise*self.dt), size=self.size)  
+        if self.type=="color": 
+            if self.color_init is False : 
+                self.noise = self.dt*np.random.normal(loc=0., scale=np.sqrt(D_noise*lam_color), size=self.size)
+                self.color_init = True 
+            else:
+                self.noise = self.noise - self.dt*lam_color*self.noise \
+                    +lam_color*np.random.normal(loc=0., scale=np.sqrt(2*D_noise*self.dt), size=self.size)
+                self.noise = self.dt*self.noise
+
+            noise = self.noise
+
+        return noise
+    
+
+# 计算同步因子
+class cal_synFactor:
+    def __init__(self, Tn, num):
+        self.Tn = Tn    # 计算次数
+        self.n = num    # 矩阵大小
+        self.count = 0  # 统计计算次数
+        # 初始化计算过程
+        self.up1 = 0
+        self.up2 = 0
+        self.down1 = torch.zeros(num)
+        self.down2 = torch.zeros(num)
+
+    def __call__(self, x):
+        F = torch.mean(x)
+        self.up1 += F*F/self.Tn
+        self.up2 += F/self.Tn
+        self.down1 += x*x/self.Tn
+        self.down2 += x/self.Tn
+        self.count += 1     # 计算次数叠加
+
+    def return_syn(self):
+        if self.count != self.Tn:
+            print(f"输入计算次数{self.Tn},实际计算次数{self.count}") 
+        down = torch.mean(self.down1-self.down2**2)
+        if down>-0.000001 and down<0.000001:
+            return 1.
+        up = self.up1-self.up2**2
+
+        return up/down
+    
+    def reset(self):
+        self.__init__(self.Tn, self.n)
+
+
+# 延迟存储器
+class delayer:
+    def __init__(self, num, Tn):
+        self.n = num                          # 延迟变量数量
+        self.delayLong = Tn                   # 延迟时长    
+        self.delay = np.zeros((num, Tn+1))    # 延迟存储矩阵
+        self.k = 0                            # 指针
+        self.delay_o = np.zeros(num)          # 需要的延迟
+
+    def __call__(self, x):
+        # 更新延迟器
+        if self.k == self.delayLong+1:     
+            self.k = 0
+        if self.k == 0:                     
+            self.delay[:, self.delayLong] = x
+        else:                                       
+            self.delay[:, self.k-1] = x 
+        # 输出延迟值
+        self.delay_o = self.delay[:, self.k]    
+        self.k += 1             # 前进指针
+
+        return self.delay_o
+         
+    def reset(self, Tn=None):
+        if Tn is not None:
+            self.__init__(self.n, Tn)
+
+
+# ISI(interspike interval)
+class ISIer:
+    def __init__(self, th_up=0, th_down=0, max=-70):
+        ''' 
+        HH可以设置为
+        th_up=0, th_down=0, max=-70.0
+        ''' 
+        self.reset_init(th_up=th_up, th_down=th_down, max=max)
+        self.pltx = []
+        self.plty = [] 
+
+    def reset_init(self, th_up, th_down, max):
+        self.th_up = th_up          # 阈上值
+        self.th_down = th_down      # 阈下值
+        self.max_init = max         # 初始最大值
+        self.max = max              # 初始变化最大值
+        self.flag = 0               # 放电标志
+        self.nn = 0                 # 峰的个数
+        self.T_pre = 0              # 前峰时间
+        self.T_post = 0             # 后峰时间
+
+    def __call__(self, x, t, y):
+        # 进入之前请初始化数据
+        # x:测量ISI的值，y:变化的值，t:理论运行时间
+        if x>self.th_up and self.flag==0:
+            self.flag = 1
+        if self.flag==1 and x>self.max:
+            self.max = x
+            self.T_post = t
+        if x<self.th_down and self.flag==1:
+            self.flag = 0
+            self.nn += 1
+            if self.nn>2:
+                ISI = self.T_post-self.T_pre
+                self.pltx.append(y)
+                self.plty.append(ISI)
+            self.T_pre = self.T_post
+            self.max = self.max_init        # 初始化
+
+    def plt_ISI(self, markersize=.8, color="k"):
+        plt.plot(self.pltx, self.plty, "o", 
+                 markersize=markersize, color=color)
+        
+    def reset(self):
+        self.reset_init(th_up=self.th_up, th_down=self.th_down 
+                        ,max=self.max_init)
+
+
---
+++ models/utils/utils_plot.py
@@ -0,0 +1,251 @@
+device = "cpu"
+import numpy
+import cupy
+if device == "gpu":
+    np1 = numpy
+    np = cupy
+else:   
+    np1 = numpy
+    np = numpy
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from matplotlib import colormaps  
+
+custom_colors = {
+                'blue': (0, 0.45, 0.7),
+                'red': (0.57, 0, 0),
+                'orange': (0.9, 0.6, 0),
+                'vermilion': (0.8, 0.4, 0),
+                'bluish_green': (0, 158./255, 115./255),
+                'yellow': (0.95, 0.9, 0.25),
+                'sky_blue': (86./255, 180./255, 233./255),
+                'pink': (0.8, 0.6, 0.7),
+                'light_blue': (109./255, 182./255, 1.),
+                'special_green': (88./255, 180./255, 109./255),
+                'teacher_green': (0., 146./255, 146./255)
+                }
+
+
+# 图像色彩梯度处理
+class Cmap:
+    '''
+    colormap的处理
+    颜色的获取可去查看：
+        1、https://matplotlib.org/2.0.2/gallery.html#color
+        2、https://mycolor.space/(获取颜色的三原色版本)
+    '''
+    @staticmethod
+    def hex2RGB(*args):
+        '''
+        例如：'#d16ba5' --> (0.8196078431372549, 0.4196078431372549, 0.6470588235294118)
+        '''
+        custom_colors = []
+        for rgb_color in args:
+            custom_colors.append(mcolors.hex2color(rgb_color))
+        return custom_colors
+    
+    @staticmethod
+    def custom_cmap(custom_colors):
+        '''
+        从一个RGB颜色列表中获取cmap
+        如：自定义jet颜色
+            cmap_colors = [
+                (0, 0, 0.5),   # 深蓝色
+                (0, 0, 1),     # 蓝色
+                (0, 1, 1),     # 青色
+                (1, 1, 0),     # 黄色
+                (1, 0, 0),     # 红色
+                (0.5, 0, 0)    # 深红色
+            ]
+        '''
+        return mcolors.LinearSegmentedColormap.from_list('custom_cmap', custom_colors)
+    
+    @staticmethod
+    def get_cmap(cmap='jet'):
+        '''
+        # Have colormaps separated into categories:
+        # http://matplotlib.org/examples/color/colormaps_reference.html
+        cmaps = [('Perceptually Uniform Sequential', [
+            'viridis', 'plasma', 'inferno', 'magma']),
+         ('Sequential', [
+            'Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds',
+            'YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu',
+            'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn']),
+         ('Sequential (2)', [
+            'binary', 'gist_yarg', 'gist_gray', 'gray', 'bone', 'pink',
+            'spring', 'summer', 'autumn', 'winter', 'cool', 'Wistia',
+            'hot', 'afmhot', 'gist_heat', 'copper']),
+         ('Diverging', [
+            'PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu',
+            'RdYlBu', 'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic']),
+         ('Qualitative', [
+            'Pastel1', 'Pastel2', 'Paired', 'Accent',
+            'Dark2', 'Set1', 'Set2', 'Set3',
+            'tab10', 'tab20', 'tab20b', 'tab20c']),
+         ('Miscellaneous', [
+            'flag', 'prism', 'ocean', 'gist_earth', 'terrain', 'gist_stern',
+            'gnuplot', 'gnuplot2', 'CMRmap', 'cubehelix', 'brg', 'hsv',
+            'gist_rainbow', 'rainbow', 'jet', 'nipy_spectral', 'gist_ncar'])]
+        '''
+        return colormaps.get_cmap(cmap)
+    
+
+# 画放电点图
+class plot_firing_raster:
+    def __init__(self, startTime=None, endTime=None):
+        self.startTime = startTime
+        self.endTime = endTime
+        self.pltPlace = []
+        self.pltTime = []
+
+    def __call__(self, flag, t):
+        # 输入放电标志和运行时间
+        if self.startTime is not None:
+            if t>=self.startTime and t<=self.endTime:
+                flag = flag.astype(int)
+                firingPlace = list(np.where(flag>0)[0]) # 放电的位置
+                lens = len(firingPlace)                 # 放电位置的数量
+                self.pltPlace.extend(firingPlace)       # 记录放电位置
+                self.pltTime.extend([t]*lens)           # 记录放电时间
+        else:
+            flag = flag.astype(int)
+            firingPlace = list(np.where(flag>0)[0]) # 放电的位置
+            lens = len(firingPlace)                 # 放电位置的数量
+            self.pltPlace.extend(firingPlace)       # 记录放电位置
+            self.pltTime.extend([t]*lens)           # 记录放电时间
+
+    def plot_raster(self, markersize=.05, color=custom_colors['vermilion']):
+        # plt.scatter(self.pltTime, self.pltPlace, 
+        #             s=.8, c=custom_colors['light_blue'])
+        plt.plot(self.pltTime, self.pltPlace, "o", 
+                 markersize=markersize, color=color)
+    
+    def reset(self):
+        self.__init__(self.startTime, self.endTime)
+        
+
+# 画线条图
+class plot_line:
+    def __init__(self):
+        self.x = []
+        self.y = []
+    
+    def __call__(self, x, y):
+        self.x.append(x)
+        self.y.append(y)
+
+    def plot(self):
+        plt.plot(self.x, self.y)
+
+
+# 画误差阴影图
+def plot_errorFill(ax, x, y_mean, y_error, color="b", alpha=0.2, label=None):
+    """
+    ax      : 画图的轴体
+    x       : x轴的数据
+    y_mean  : y轴平均值数据
+    y_error : y轴误差数据
+    color   : 颜色（默认蓝色）
+    alpha   : 阴影的透明度
+    """
+    ax.plot(x, y_mean, "-", color=color, label=label)
+    ax.fill_between(x, y_mean-y_error, y_mean+y_error, color=color, alpha=alpha)
+
+
+# 画误差棒图
+def plot_errorbar(ax, x, y_mean, y_error, color='b', capsize=5, 
+                  linewidth=2.5, markersize=5, label=None):
+    """
+    ax      : 画图的轴体
+    x       : x轴的数据
+    y_mean  : y轴平均值数据
+    y_error : y轴误差数据
+    label   : 标签
+    color   : 颜色（默认蓝色）
+    capsize : 误差棒上下两端条的大小
+    linewidth  : 线的宽度
+    markersize : 标记的宽度（默认为"o"的大小）
+    """
+    ax.errorbar(x, y_mean, yerr=y_error, capsize=capsize, 
+                linestyle='-', linewidth=linewidth, 
+                marker='o', markersize=markersize, 
+                label=label, color=color, alpha=0.8)
+
+
+# 设置子图属性
+def set_ax(ax, title=None, title_x=None, 
+           xlim=None, xlabel=None, xlabel_coords=None, 
+           ylim=None, ylabel=None, ylabel_coords=None, 
+           font_size=None):
+    '''
+    --> 若需要单独设置，将命令单独拿出去运行(查找命令改变更多)
+    ax : 轴体
+    title : 标题
+    title_x : 标题的水平位置
+    xlim : x轴的范围
+    xlabel : x轴标签
+    xlabel_coords : x轴标签的坐标(x, y)
+    ylim : y轴的范围
+    ylabel_coords : y轴标签的坐标(x, y)
+    ylabel : y轴标签
+    font_size = [tick_size, label_size, title_size]
+    '''
+    # 设置字体
+    font = {'family': 'Times New Roman', 'style': 'italic'}
+    font1 = {'family': 'Times New Roman'}
+
+    # 设置字体大小
+    tick_size = 12           # 刻度大小
+    label_size = 22         # 标签大小
+    title_size = 24         # 标题大小
+    if font_size is not None: font_size = font_size
+    else: font_size = [tick_size, label_size, title_size]
+
+    if title is not None:   ax.set_title(title, fontsize=font_size[2], fontdict=font1) # 调整标题垂直位置使用：pad=20
+    if title_x is not None:   ax.title.set_position([title_x, 1])  # 调整标题位置
+
+    if xlim is not None:    ax.set_xlim(*xlim)
+    if xlabel is not None:    ax.set_xlabel(xlabel, fontsize=font_size[1], fontdict=font)
+    if xlabel_coords is not None:    ax.xaxis.set_label_coords(*xlabel_coords) # 设置x轴与x标签的相对位置
+    
+    if ylim is not None:    ax.set_ylim(*ylim)
+    if ylabel is not None:    ax.set_ylabel(ylabel, fontsize=font_size[1], fontdict=font)
+    if ylabel_coords is not None:    ax.yaxis.set_label_coords(*ylabel_coords) # 设置y轴与y标签的相对位置
+
+    ax.tick_params(labelsize=font_size[0])       # axis={'x', 'y', 'both'},
+                         
+
+# 调节轴体的相对位置（上下左右移动）
+def move_ax(ax, m_lr=0, m_ud=0):
+    '''
+    ax :  轴体
+    m_lr: 向左向右移动(+:右, -:左) 0-1的相对位置
+    m_ud: 向上向下移动(+:上, -:下) 0-1的相对位置
+
+    注意：
+        1、画图中使用了fig.tight_layout, fig.subplots_adjust, 在后面使用移动
+        2、调节颜色棒的位置，输入colorbar.ax的轴体
+
+    '''
+    position = ax.get_position()    # 获取子轴相对位置
+
+    # 移动子图位置（向右和向上移动）
+    mrl = m_lr
+    mud = m_ud
+    new_x0 = position.x0 + mrl  # 向右移动
+    new_y0 = position.y0 + mud  # 向上移动
+    new_x1 = position.x1 + mrl  # 向右移动
+    new_y1 = position.y1 + mud  # 向上移动
+
+    ax.set_position([new_x0, new_y0, new_x1 - new_x0, new_y1 - new_y0])
+
+
+if __name__=="__main__":
+    fig, ax = plt.subplots()
+    set_ax(ax, title=None, title_x=None, 
+           xlim=None, xlabel=None, xlabel_coords=None, 
+           ylim=None, ylabel=None, ylabel_coords=None, 
+           font_size=None)
+    plt.show()
+
+

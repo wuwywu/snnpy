5c0cc0f39e2bfc39c4c0ab18877de6d5768dccb8
wuwywu <676425940@qq.com>
2024-03-11 11:46:01 +0800

    加入了时间延迟的突触结构

--- models/synapse/synapse.py
+++ models/synapse/synapse.py
@@ -87,3 +87,85 @@ class synbase:
         g_syn = alpha * np.expand_dims((self.e - post_mem), axis=1)
         Isyn = (self.w * self.conn * g_syn).sum(axis=1)  # 0维度--post，1维度--pre
         return Isyn
+
+
+# 包含突触传输延迟
+class synbaseLayer:
+    """
+    pre: 突触前神经元
+    post: 突触后神经元
+    conn: 连接矩阵
+    synType: 突触类型["electr", "chem_Alpha"]
+    delayer: 延迟器
+    """
+    def __init__(self, pre, post, conn=None, synType="electr", delayer=None):
+        self.pre = pre      # 突触前神经元
+        self.post = post    # 突触后神经元
+        # self.iself = (self.pre is self.post)  # 判断是否只有一个子网络（突触前和和突触后神经元是一个）
+        self.conn = conn    # 连接矩阵
+        self.dt = post.dt  # 计算步长
+        # 选着突触类型
+        self.synType = synType
+        self.delayer = delayer
+        if self.synType == "electr":
+            self.syn = self.syn_electr  # 电突触
+        elif self.synType == "chem_Alpha":
+            self.syn = self.syn_chem_Alpha  # Alpha_化学突触
+        self._fparams()
+        self._fvars()
+
+    def _fparams(self):
+        # 0维度--post，1维度--pre
+        self.w = .1*np.ones((self.post.num, self.pre.num)) # 设定连接权重
+        # Alpha_化学突触的系数
+        self.e = 0  # 化学突触的平衡电位
+        self.tau_syn = 2  # 化学突触的时间常数
+
+    def _fvars(self):
+        self.t = self.post.t
+
+    def __call__(self):
+        """
+        开头和结尾更新时间（重要）
+        self.t = self.post.t
+        self.t += self.dt
+        """
+        # 保证syn不管何时创建都能与突触后有相同的时间
+        self.t = self.post.t    # 这个是非常重要的
+
+        I_post = self.syn()     # 突触后神经元接收的突触电流
+        # if not self.iself:  # 如果是两个网络则更新pre和post，否者只更新post
+        #     self.pre()  # 突触前神经元
+        # self.post(I)  # 突触后神经元
+
+        self.t += self.dt  # 时间前进
+
+        return I_post
+
+    def syn_electr(self, pre_mem=None, post_mem=None):
+        """
+        args:
+            pre_mem: 突触前的膜电位
+            post_mem: 突触后的膜电位
+        """
+        if pre_mem is None: pre_mem = self.pre.mem
+        if post_mem is None: post_mem = self.post.mem
+        pre_mem = self.delayer(self.pre.mem)    # 存储延迟，并给出延迟的值
+        vj_vi = pre_mem-np.expand_dims(post_mem, axis=1)   # pre减post
+        Isyn = (self.w*self.conn*vj_vi).sum(axis=1)  # 0维度--post，1维度--pre
+
+        return Isyn
+
+    def syn_chem_Alpha(self, pre_fire_t=None, post_mem=None):
+        """
+        args:
+            pre_fire_t: 突触前的放电时间
+            post_mem： 突触后的膜电位
+        """
+        if pre_fire_t is None: pre_fire_t = self.pre.firingTime
+        if post_mem is None: post_mem = self.post.mem
+        alpha = (self.t - pre_fire_t) / self.tau_syn * np.exp((pre_fire_t-self.t)/self.tau_syn)
+        alpha = self.delayer(alpha)    # 存储延迟，并给出延迟的值
+        g_syn = alpha * np.expand_dims((self.e - post_mem), axis=1)
+        Isyn = (self.w * self.conn * g_syn).sum(axis=1)  # 0维度--post，1维度--pre
+        return Isyn

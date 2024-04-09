41ce7a61606576f6b3e1ab1cd66949c2fc9b7af6
wuwywu <676425940@qq.com>
2024-04-09 10:19:00 +0800

    存储各类函数的动力学模型，以及其jacobian矩阵

--- models/utils/neuron_jacobian.py
+++ models/utils/neuron_jacobian.py
@@ -34,6 +34,36 @@ def jac(x, t):
     res[2, 0], res[2, 1], res[2, 2] = r*s, 0, -r
     return res
 
+# Hindmarsh-Rose(HR) ncluding the magnetic flux variable 模型
+def HR_mag(x, t):
+    res = np.zeros_like(x)
+    # 常数
+    a, b = 1, 3
+    Iex = 3.2
+    c, d = 1, 5
+    r, s, x_rest = .006, 4, -1.6
+    k0, k1, k2 = 1, 1, 0.5
+    # 输出函数变量
+    res[0] = x[1] - a*x[0]**3 + b*x[0]**2 - x[2] + Iex - k0*(0.01+0.06*x[3]**2)*x[0]
+    res[1] = c - d*x[0]**2 - x[1]
+    res[2] = r*(s*(x[0]-x_rest) - x[2])
+    res[3] = k1*x[0] - k2*x[3]
+    return res
+
+def jac(x, t):
+    res = np.zeros((x.shape[0], x.shape[0]))
+    # 常数
+    a, b = 1, 3
+    Iex = 3.2
+    c, d = 1, 5
+    r, s, x_rest = .006, 4, -1.6
+    k0, k1, k2 = 1, 1, 0.5
+    # 输出函数变量
+    res[0, 0], res[0, 1], res[0, 2], res[0, 3] = -3*a*x[0]**2 + 2*b*x[0] - k0*(0.01+0.06*x[3]**2) , 1, -1, k0*2*0.06*x[3]*x[0]
+    res[1, 0], res[1, 1], res[1, 2], res[1, 3] = -2*d*x[0], -1, 0, 0
+    res[2, 0], res[2, 1], res[2, 2], res[2, 3] = r*s, 0, -r, 0
+    res[3, 0], res[3, 1], res[3, 2], res[3, 3] = k1, 0, 0, -k2
+    return res
 
 # FitzHugh-Nagumo(FHN) 模型
 def FHN(x, t):

70d1fc574bad1c9dc9780659977face65d09af70
wuwywu <676425940@qq.com>
2024-04-10 10:17:36 +0800

    研究中常用到的FHN的另一种形式

--- models/nodes/FHN.py
+++ models/nodes/FHN.py
@@ -72,6 +72,59 @@ class FHN(Neurons):
         self.t += self.dt  # 时间前进
 
 
+class FHN2(Neurons):
+    """
+    N : 建立神经元的数量
+    method ： 计算非线性微分方程的方法，（"euler", "rk4"）
+    dt ： 计算步长
+    神经元的膜电位都写为：mem
+    """
+    def __init__(self, N=1, method="euler", dt=0.01):
+        super().__init__(N, method=method, dt=dt)
+        # self.num = N  # 神经元数量
+        self._params()
+        self._vars()
+
+    def _params(self):
+        self.a = 0.5
+        self.b = 0.05
+        self.Iex = 0  # 恒定的外部激励
+        self.th_up = 1  # 放电阈值
+        self.th_down = 1  # 停止放电阈值
+
+    def _vars(self):
+        # 模型变量
+        self.mem = np.zeros(self.num)
+        self.y = np.zeros(self.num)
+        self.N_vars = 2 # 变量的数量
+        # self.t = 0  # 运行时间
+
+    def _fhn(self, I):
+        dmem_dt = (self.mem - self.mem ** 3 / 3 - self.y + I[0])/self.b
+        dy_dt = self.mem + self.a + I[1]
+        return dmem_dt, dy_dt
+
+    def __call__(self, Io=0, axis=[0]):
+        """
+        args:
+            Io: 输入到神经元模型的外部激励，
+                shape:
+                    (len(axis), self.num)
+                    (self.num, )
+                    float
+            axis: 需要加上外部激励的维度
+                list
+        """
+        I = np.zeros((self.N_vars, self.num))
+        I[0, :] = self.Iex        # 恒定的外部激励
+        I[axis, :] += Io
+
+        self.method(self._fhn, I, self.mem, self.y)  #
+        self._spikes_eval(self.mem)  # 放电测算
+
+        self.t += self.dt  # 时间前进
+
+
 if __name__ == "__main__":
     N = 2
     method = "euler"  # "rk4", "euler"

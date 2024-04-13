292c71827a3b08a5582be0aafed42875ab2ca485
wuwywu <676425940@qq.com>
2024-04-13 19:37:01 +0800

    研究中常用到的FHN的另一种形式(包含磁通量项)

--- models/nodes/FHN.py
+++ models/nodes/FHN.py
@@ -125,10 +125,77 @@ class FHN2(Neurons):
         self.t += self.dt  # 时间前进
 
 
+class FHN2_mag(Neurons):
+    """
+    N : 建立神经元的数量
+    method ： 计算非线性微分方程的方法，（"euler", "rk4"）
+    dt ： 计算步长
+    神经元的膜电位都写为：mem
+
+    包含 magnetic flux 项的 FHN 模型
+    """
+    def __init__(self, N=1, method="euler", dt=0.01):
+        super().__init__(N, method=method, dt=dt)
+        # self.num = N  # 神经元数量
+        self._params()
+        self._vars()
+
+    def _params(self):
+        self.a = 0.5
+        self.b = 0.05
+        self.Iex = 0  # 恒定的外部激励
+        self.th_up = 1  # 放电阈值
+        self.th_down = 1  # 停止放电阈值
+        # 磁通量项的常数
+        self.k = .1    # 反馈增益，其中k桥接了磁通或磁场对膜电位的耦合和调制
+        self.k1 = .1    # K1计算了离子在细胞中传输引起的电磁感应效应
+        self.k2 = 1.    # K2描述了极化度和磁化强度，也被认为是调节磁通饱和的物理参数
+        self.alpha = 0.1
+        self.beta = 0.1
+
+    def _vars(self):
+        # 模型变量
+        self.mem = np.zeros(self.num)
+        self.y = np.zeros(self.num)
+        self.phi = np.zeros(self.num)
+        self.N_vars = 3 # 变量的数量
+        # self.t = 0  # 运行时间
+
+    def _fhn(self, I):
+        rho = self.alpha + 3*self.beta*(self.phi**2)
+        dmem_dt = (self.mem - self.mem ** 3 / 3 - self.y + I[0]
+                   - self.k*rho*self.mem)/self.b
+        dy_dt = self.mem + self.a + I[1]
+        dphi_dt = self.k1*self.mem - self.k2*self.phi + I[2]
+        return dmem_dt, dy_dt, dphi_dt
+
+    def __call__(self, Io=0, axis=[0]):
+        """
+        args:
+            Io: 输入到神经元模型的外部激励，
+                shape:
+                    (len(axis), self.num)
+                    (self.num, )
+                    float
+            axis: 需要加上外部激励的维度
+                list
+        """
+        I = np.zeros((self.N_vars, self.num))
+        I[0, :] = self.Iex        # 恒定的外部激励
+        I[axis, :] += Io
+
+        self.method(self._fhn, I, self.mem, self.y, self.phi)  #
+        self._spikes_eval(self.mem)  # 放电测算
+
+        self.t += self.dt  # 时间前进
+
+
 if __name__ == "__main__":
     N = 2
-    method = "euler"  # "rk4", "euler"
-    models = FHN(N=N, method=method)
+    method = "rk4"  # "rk4", "euler"
+    # models = FHN(N=N, method=method)
+    models = FHN2_mag(N=N, method=method)
+    models.k = -10.
 
     time = []
     mem = []
@@ -137,7 +204,7 @@ if __name__ == "__main__":
     for i in range(10000):
         # I = np.random.rand(2, N)*0.01
         # I = np.zeros((2, N))
-        models(Io=1)
+        models(Io=0)
         time.append(models.t)
         mem.append(models.mem.copy())
         se(models.t, models.flaglaunch)
@@ -149,3 +216,4 @@ if __name__ == "__main__":
     # print(se.Tspike_list)
 
     plt.show()
+

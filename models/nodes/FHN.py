af592cb8c1fd35d9adab418edf1ac12024caaa9a
wuwywu <676425940@qq.com>
2024-05-08 12:18:57 +0800

    memristor-based FHN (mFHN)

--- models/nodes/FHN.py
+++ models/nodes/FHN.py
@@ -190,29 +190,110 @@ class FHN2_mag(Neurons):
         self.t += self.dt  # 时间前进
 
 
+# ================================= memristor-based FHN (mFHN) =================================
+# references: Y. Xie, Z. Ye, X. Li, X. Wang, Y. Jia, A novel memristive neuron model and its energy characteristics,
+# Cogn. Neurodynamics. https://doi.org/10.1007/s11571-024-10065-5
+class mFHN(Neurons):
+    """
+    N : 建立神经元的数量
+    method ： 计算非线性微分方程的方法，（"euler", "rk4"）
+    dt ： 计算步长
+    神经元的膜电位都写为：mem
+    """
+    def __init__(self, N=1, method="euler", dt=0.01):
+        super().__init__(N, method=method, dt=dt)
+        # self.num = N  # 神经元数量
+        self._params()
+        self._vars()
+
+    def _params(self):
+        self.a = 0.3
+        self.b = 1.0
+        self.c = 0.1
+        # 电磁的参数
+        self.k1 = 1.0
+        self.k2 = 1.8
+        self.alpha = 0.1
+        self.beta = 0.3
+        # 外部信号源参数
+        self.A = 0.8
+        self.omega = 0.9
+
+        self.Iex = 0  # 恒定的外部激励
+        self.th_up = 1  # 放电阈值
+        self.th_down = 1  # 停止放电阈值
+
+    def _vars(self):
+        # 模型变量
+        self.mem = 0.2*np.ones(self.num)
+        self.y = 0.1*np.ones(self.num)
+        self.phi = 0.05*np.ones(self.num)
+        self.N_vars = 3  # 变量的数量
+        # self.t = 0  # 运行时间
+
+    def _mfhn(self, I):
+        us = 0.2 + self.A * np.cos(self.omega * self.t) # 外部信号源
+        rho = self.alpha + self.beta * (self.phi ** 2)
+        dmem_dt = -rho * (self.mem - us) + self.mem - self.mem ** 3 / 3 - self.y + I[0]
+        dy_dt = self.c * (self.mem + self.a - self.b * self.y) + I[1]
+        dphi_dt = -self.k1 * (self.mem - us) - self.k2 * self.phi + I[2]
+
+        return dmem_dt, dy_dt, dphi_dt
+
+    def __call__(self, Io=0, axis=[0]):
+        """
+        args:
+            Io: 输入到神经元模型的外部激励，
+                shape:
+                    (len(axis), self.num)
+                    (self.num, )
+                    float
+            axis: 需要加上外部激励的维度
+                list
+        """
+        I = np.zeros((self.N_vars, self.num))
+        I[0, :] = self.Iex        # 恒定的外部激励
+        I[axis, :] += Io
+
+        self.method(self._mfhn, I, self.mem, self.y, self.phi)  #
+        self._spikes_eval(self.mem)  # 放电测算
+
+        self.t += self.dt  # 时间前进
+
+
 if __name__ == "__main__":
     N = 2
     method = "rk4"  # "rk4", "euler"
     # models = FHN(N=N, method=method)
-    models = FHN2_mag(N=N, method=method)
-    models.k = -10.
+    # models = FHN2_mag(N=N, method=method)
+    # models.k = -10.
+    models = mFHN(N=N, method=method)
+    models.omega = 0.01
 
     time = []
     mem = []
+    y = []
     se = spikevent(N)
 
-    for i in range(10000):
+    for i in range(50000):
+        models()
+
+    for i in range(200000):
         # I = np.random.rand(2, N)*0.01
         # I = np.zeros((2, N))
-        models(Io=0)
+        models()
         time.append(models.t)
         mem.append(models.mem.copy())
+        y.append(models.y.copy())
         se(models.t, models.flaglaunch)
 
-    ax1 = plt.subplot(211)
+    plt.figure(figsize=(6, 12))
+    ax1 = plt.subplot(311)
     plt.plot(time, mem)
-    plt.subplot(212, sharex=ax1)
+    plt.subplot(312, sharex=ax1)
     se.pltspikes()
+    ax3 = plt.subplot(313)
+    plt.plot(mem, y)
     # print(se.Tspike_list)
 
     plt.show()

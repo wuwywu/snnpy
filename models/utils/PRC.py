04049a5025ed7a5e6c9e822be44510ccadf0d9c4
wuwywu <676425940@qq.com>
2024-08-11 13:42:05 +0800

    开始写 相位响应曲线 代码

---
+++ models/utils/PRC.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+# Author    : WuY<wuyong@mails.ccnu.edu.com>
+# Datetime  : 2024/8/11
+# User      : WuY
+# File      : PRC.py
+# 相位响应曲线 (Phase Response Curves, PRCs)
+
+import numpy as np
+import matplotlib.pyplot as plt
+from numba import jit, prange
+
+# 定义 PRC 所使用的突触形式
+
+
+
+
+
+
+

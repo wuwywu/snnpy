d861b28900c823e17da6b6323d32db5d48e520ca
wuwywu <676425940@qq.com>
2024-08-26 14:54:11 +0800

    用numba做并行的演算法

--- models/utils/algorithm.py
+++ models/utils/algorithm.py
@@ -3,13 +3,15 @@
 # Datetime  : 2024/8/20
 # User      : WuY
 # File      : algorithm.py
-# 给出演算法计算节点变化
+# 给出演算法计算节点变化, 使用numba加速运行，可以做并行运算
 
 
 import numpy as np
 import matplotlib.pyplot as plt
+from numba import njit, prange
 
-def f(x, t):
+@njit
+def f(x, t, *args):
     """
     args:
         x (numpy.ndarray) : 状态变量
@@ -22,14 +24,38 @@ def f(x, t):
 
 
 # 四阶 龙格库塔(RK4)
-def rk4(f, x, t, dt):
+@njit
+def rk4(f, x, t, dt, *args):
     '''
     使用 RK4 方法计算一个时间步后系统的状态。
+    args:
+        f : 函数
+        x (numpy.ndarray) : 状态变量
+        t (float) : 运行时间
+    return:
+        res (numpy.ndarray) : 状态变量矩阵
     '''
-    k1 = f(x, t)
-    k2 = f(x + (dt / 2.) * k1, t + (dt / 2.))
-    k3 = f(x + (dt / 2.) * k2, t + (dt / 2.))
-    k4 = f(x + dt * k3, t + dt)
+    k1 = f(x, t, *args)
+    k2 = f(x + (dt / 2.) * k1, t + (dt / 2.), *args)
+    k3 = f(x + (dt / 2.) * k2, t + (dt / 2.), *args)
+    k4 = f(x + dt * k3, t + dt, *args)
     x = x + (dt / 6.) * (k1 + 2 * k2 + 2 * k3 + k4)
 
     return x
+
+@njit
+def euler(f, x, t, dt, *args):
+    '''
+    使用 euler 方法计算一个时间步后系统的状态。
+    args:
+        f : 函数
+        x (numpy.ndarray) : 状态变量
+        t (float) : 运行时间
+    return:
+        res (numpy.ndarray) : 状态变量矩阵
+    '''
+    dx_dt = f(x, t, *args)
+    x = x + dt * dx_dt
+
+    return x
+
